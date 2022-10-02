268d33e43b7300812564c782dd1781d81933c9da
Felix Niemeyer <niemeyer.felix@gmail.com>
2022-10-02 18:30:14 +0200

    writing overview page

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,6 +1,29 @@
+<script setup lang=ts>
+</script>
+
 <template>
   <div class="overview">
     <h1>Overview</h1>
+    <h2>Share resources</h2>
+    <p>If you have something valuable, that you don't use youself all of the time, you can share it with others and earn money for it. Some examples for resources you could share are...</p>
+    <ul>
+      <li>rooms or houses</li>
+      <li>machines or tools</li>
+      <li>cars, bikes and trailers</li>
+    </ul>
+    <p>Chershare makes the process of renting out resources as easy as possible. Earn crypto by renting out your resources. </p> 
+    <a></a>
+    <RouterLink to="/res/create">Create a resource</RouterLink>
+    <h2>Rent resources</h2>
+    <p>A holiday home, a place for a party, a projector, or a mobile sawmill, whatever you need, there is a good chance, you will find it on Chershare.</p>
+    <a></a>
+    <RouterLink to="/bookings">Search for a resource</RouterLink>
+    <h2>Manage resources</h2>
+    <p>If you are already sharing resources, you can find data on the dashboard and edit resources</p>
+    <RouterLink to="/res/manage/asd">here</RouterLink>
+    <h2>Manage bookings</h2>
+    <p>If you have booked resources look up the details</p>
+    <RouterLink to="/res/manage/asd">here</RouterLink>
   </div>
 </template>
 
