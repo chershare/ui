868e4dfcc710299cef457e1f734b6b874c681ad7
Felix Niemeyer (on linode ajam) <niemeyer.feix@gmail.com>
2022-11-23 11:12:50 +0000

    put default mediaserver into config

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,8 +1,10 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
+import config from '@/config'
+
 export const useSettingsStore = defineStore("settings", () => {
-  const mediaServerUrl = ref("http://localhost:8000");
+  const mediaServerUrl = ref(config.mediaServerDefaultHost);
   // const doubleCount = computed(() => count.value * 2);
   // function increment() {
   //   count.value++;
